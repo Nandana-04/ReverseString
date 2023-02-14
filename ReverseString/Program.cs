32779c72fee7f5c6b7e13c75a998889e14760617
nandana.s <nandana.s@NANDANAS>
2023-02-14 16:17:27 +0530

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string : ");
+            string input = Console.ReadLine();
+            char[] charArray = new char[input.Length];
+            int j = input.Length - 1;
+            for (int i = 0; i < input.Length; i++)
+            { 
+                charArray[i] = input[j];
+                j--; 
+            }
+            string reversed = new string(charArray);
+            Console.WriteLine("Reversed string :" + reversed);
+        }
+    }
+}
+    
